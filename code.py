84b6d77ee3be9318499130986676e034c9b90ed7
Vincent Tian <vincenttian16@gmail.com>
2014-10-21 15:07:13 -0700

    add code.py for non ipython code

---
+++ code.py
@@ -0,0 +1,32 @@
+import csv
+
+with open('Engineering_CS101_Summer2014_Forum.csv', 'rb') as csvfile:
+    contents = csv.reader(csvfile)
+    count = 0
+    
+    master_dict = dict()
+    
+    for row in contents:
+        if len(row) < 17:
+            continue
+        if row[0][0] != '5':
+            continue
+        forum_post_id = row[0]
+        forum_uid = row[6]
+        comment = row[7]
+        votes = row[11]
+        voters = row[14]
+        comment_thread_id = row[16]
+        parent_id = row[17]
+        info = dict()
+        info['forum_uid'] = forum_uid
+        info['comment'] = comment
+        info['votes'] = votes
+        info['voters'] = voters
+        info['comment_thread_id'] = comment_thread_id
+        info['parent_id'] = parent_id
+        if 'thank' in comment or 'thx' in comment:
+            info['thanks'] = True
+        else:
+            info['thanks'] = False
+        master_dict[forum_post_id] = info
