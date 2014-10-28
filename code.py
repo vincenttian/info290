d46c985af0025426a73a4b9b41f39be90cf4f8ba
Jordeen Chang <jordeenchang@gmail.com>
2014-10-28 01:13:36 -0700

    creating date dictionary

--- code.py
+++ code.py
@@ -1,4 +1,15 @@
 import csv
+import datetime
+
+
+def valid_date(datestring):
+    try:
+        datetime.datetime.strptime(datestring, '%Y-%m-%d')
+        return True
+    except ValueError:
+        return False
+
+
 
 if __name__ == '__main__':
     with open('Engineering_CS101_Summer2014_Forum.csv', 'rb') as csvfile:
@@ -7,6 +18,7 @@ if __name__ == '__main__':
 
         master_dict = dict()
         count_dict = dict()
+        date_dict = dict()
         count_dict['question'] = 0
         count_dict['thanks'] = 0
         count_dict['sentence'] = 0
@@ -18,9 +30,10 @@ if __name__ == '__main__':
             if row[0][0] != '5':
                 continue
             forum_post_id = row[0]
-            commentType = row[2]
+            comment_type = row[2]
             forum_uid = row[6]
             comment = row[7]
+            date = row[9].split(' ')[0]
             votes = row[11]
             voters = row[14]
             comment_thread_id = row[16]
@@ -28,6 +41,7 @@ if __name__ == '__main__':
             info = dict()
             info['forum_uid'] = forum_uid
             info['comment'] = comment
+            info['date'] = date
             info['votes'] = votes
             info['voters'] = voters
             info['comment_thread_id'] = comment_thread_id
@@ -41,13 +55,28 @@ if __name__ == '__main__':
                 count_dict['question'] += 1
             if '.' in comment or '!' in comment:
                 count_dict['sentence'] += 1
-            if 'Thread' in commentType:
+            if 'Thread' in comment_type:
                 count_dict['commentThread'] += 1
             else:
                 count_dict['comment'] += 1
             master_dict[forum_post_id] = info
+        for posts in master_dict:
+            info = master_dict[posts]
+            d = info['date']
+            if valid_date(d):
+                if d in date_dict:
+                    date_dict[d] += 1
+                else:
+                    date_dict[d] = 1
+        print date_dict
         print '# of questions: ' + str(count_dict['question'])
         print '# of thanks: ' + str(count_dict['thanks'])
         print '# of sentences: ' + str(count_dict['sentence'])
         print '# of comments: ' + str(count_dict['comment'])
         print '# of commentThreads: ' + str(count_dict['commentThread'])
+
+
+
+
+
+
