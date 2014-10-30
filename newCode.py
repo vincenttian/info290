d6d1524c5c1c17a0bbbcf5a5758c5ab067b638f4
Jordeen Chang <jordeenchang@gmail.com>
2014-10-30 00:37:10 -0700

    testing vs training

--- newCode.py
+++ newCode.py
@@ -3,6 +3,8 @@ import datetime
 import collections
 from collections import defaultdict
 from scipy.stats.stats import pearsonr
+from sklearn import svm
+import numpy
 
 def valid_date(datestring):
     try:
@@ -13,10 +15,9 @@ def valid_date(datestring):
 
 
 if __name__ == '__main__':
+
     with open('Engineering_CS101_Summer2014_Forum_Sensitive.csv', 'rb') as csvfile:
         contents = csv.reader(csvfile)
-        count = 0
-
         master_dict = dict()
         for row in contents:
             if len(row) < 17:
@@ -41,8 +42,7 @@ if __name__ == '__main__':
                 # [1] = average len of posts but will temporarily hold total words
                 # [2] = num of comments
                 # [3] = num of comment threads
-                # [4] = num of votes
-                # [5] = num of questions
+                # [4] = num of questions
                 for i in range(5):
                     data.append(0)
             data[0] += 1
@@ -56,7 +56,57 @@ if __name__ == '__main__':
         #getting the average
         for user in master_dict:
             master_dict[user][1] /= master_dict[user][0]
+
+    with open('gradesAndCerts.csv', 'rb') as csvfile:
+        grades_dict = dict()
+        certified_dict = dict()
+        contents = csv.reader(csvfile)
+        for row in contents:
+            if len(row) != 3:
+                continue
+            anon_screen_name = row[0]
+            grade = row[1]
+            certified = row[2]
+            grades_dict[anon_screen_name] = grade
+            certified_dict[anon_screen_name] = certified
+            if anon_screen_name not in master_dict:
+                data = master_dict[anon_screen_name] = []
+                for i in range(5):
+                    data.append(0)
+    ordered_certified = collections.OrderedDict(sorted(certified_dict.items()))
     ordered_master_dict = collections.OrderedDict(sorted(master_dict.items()))
-    print ordered_master_dict
+    # print ordered_certified
+    print len(ordered_certified)
+    print len(ordered_master_dict)
+
+    training_features = []
+    training_scores = []
+    training_certified = []
+    testing_features = []
+    testing_scores = []
+    testing_certified = []
+    count = 0
+    for i in ordered_master_dict:
+        if count > 1000:
+            break
+        if i not in ordered_certified:
+            continue
+        if count <= 500:
+            f_array = ordered_master_dict[i]
+            training_features.append(f_array)
+            training_scores.append(grades_dict[i])
+            training_certified.append(certified_dict[i])
+        if count > 500:
+            f_array = ordered_master_dict[i]
+            testing_features.append(f_array)
+            testing_scores.append(grades_dict[i])
+            testing_certified.append(certified_dict[i])
+        count += 1
+    print len(training_features)
+    print len(testing_features)
+    clf = svm.SVC()
+    clf.fit(training_features, training_certified)
+
+
 
     # NLTK, regex
